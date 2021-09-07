fdba0f96d04d11ac925d273f064a2138f3fe2aa6
Youngerjesus <yjh930919@gmail.com>
2021-09-08 02:37:22 +0900

    Upload Project

---
+++ src/main/java/thymeleaf/app/BaseController.java
@@ -0,0 +1,17 @@
+package thymeleaf.app;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/base")
+public class BaseController {
+
+    @GetMapping
+    public String textBasic(Model model) {
+        model.addAttribute("data", "hello spring");
+        return "index";
+    }
+}
